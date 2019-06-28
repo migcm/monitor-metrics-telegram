76b323f09460a88a1c8277810f755678cd82b0d4
root <root@vps657991.ovh.net>
2019-06-28 13:17:51 +0200

    All checks working

--- checker.sh
+++ checker.sh
@@ -13,7 +13,7 @@ load_config(){
 	#Check
 		# Ram
 		RAM_CRITICAL=90
-		RAM_WARNING=80	
+		RAM_WARNING=80
 		# Swap
 		SWAP_CRITICAL=90
 		SWAP_WARNING=80
@@ -22,92 +22,92 @@ load_config(){
 		DISK_WARNING=80
  		# CPU
 		CPU_CRITICAL=90
-		CPU_WARNING=80	
+		CPU_WARNING=80
 		# Ping
-		PING_HOSTS="google.es;192.168.1.1"
+		PING_HOSTS="google.es;localhost"
 		PING_NUMBER=3
 		#Processes
-		PROCESSES_LIST="ssh;http"
+		PROCESSES_LIST="ssh;docker"
 		# Iptables
 		# Ports
-		PORTS_LIST="localhost:80;localhost:443"
+		PORTS_LIST="0.0.0.0:ssh"
 }
 
 check_ram(){
 
 	totalRam=$(free -m | awk '/Mem:/ { print $2 }')
 	freeRam=$(free -m | awk '/Mem:/ { print $4 }')
-
 	percentRam=$(bc <<< "scale=1; $freeRam*100 / $totalRam")
 
-	#convert to int
-    percentRamint=${percentRam%.*}
-
-	if (( $percentRamint > $RAM_CRITICAL ))
-        then
-                send_error " 🔴 CRITICAL 🔴 " "The ram is at $percentRam."
-        elif (( $percentRamint > $RAM_WARNING ))
-        then
-                send_error "WARNING" "The ram is at $percentRam."
-        fi
+	if (( ${percentRam%.*} > $RAM_CRITICAL  ))
+    then
+            send_error " 🔴 CRITICAL 🔴 " "The ram is at $percentRam."
+    elif (( ${percentRam%.*} > $RAM_WARNING ))
+    then
+            send_error " ❕ WARNING ❕ " "The ram is at $percentRam."
+    fi
 
 
 	# Debug mode
-	if [ $DEBUG_MODE == 1 ]
+	if (( $DEBUG_MODE == 1 ))
 	then
-                send_error "DEBUG" "The ram has been checked: $percentRam %."
+                send_error " 📢 DEBUG 📢 " "The ram has been checked: $percentRam %."
                 echo "DEBUG - The ram has been checked: $percentRam %."
 	fi
 }
 
 check_swap(){
 
-    totalSwap=$(free -m | awk '/Swap:/ { print $2 }')
-    freeSwap=$(free -m | awk '/Swap:/ { print $3 }')
+	totalSwap=$(free -m | awk '/Swap:/ { print $2 }')
+	freeSwap=$(free -m | awk '/Swap:/ { print $3 }')
 
-    percentSwap=$(bc <<< "scale=1; $freeSwap*100 / $totalSwap")
-
-	if [ "$percentSwap" > $SWAP_CRITICAL ]
-        then
-                send_error "CRITICAL" "The swap is at $percentSwap."
-        elif [ "$percentSwap" > $SWAP_WARNING  ]
+	if (( "$totalSwap" > 0 ))
 	then
-                send_error "WARNING" "The swap is at $percentSwap."
+		percentSwap=$(bc <<< "scale=1; $freeSwap*100 / $totalSwap")
+	else
+		percentSwap=0
+	fi
+
+	if (( ${percentSwap%.*} > $SWAP_CRITICAL ))
+        then
+                send_error " 🔴 CRITICAL 🔴 " "The swap is at $percentSwap."
+        elif (( ${percentSwap%.*} > $SWAP_WARNING  ))
+	then
+                send_error " ❕ WARNING ❕ " "The swap is at $percentSwap."
 	fi
 
 
 	# Debug mode
-	if [ $DEBUG_MODE == 1 ]
-        then
-                send_error "DEBUG" "The swap has been checked: $percentSwap %."
-                echo "DEBUG - The swap has been checked: $percentSwap %."
-        fi
+	if (( $DEBUG_MODE == 1 ))
+    then
+            send_error " 📢 DEBUG 📢 " "The swap has been checked: $percentSwap %."
+            echo "DEBUG - The swap has been checked: $percentSwap %."
+    fi
 }
 
 
 check_disk(){
 
-	totalDisk=$(df -h --output=size -x tmpfs -x devtmpfs)
-	freeDisk=$(df -h --output=avail -x tmpfs -x devtmpfs)
-	percentDisk=$(df -h --output=avail -x tmpfs -x devtmpfs | grep -o '[0-9]')
-	
-	if [ "$percentDisk" > $DISK_CRITICAL ]
+	percentDisk=$(df -h | grep sd | awk '{print $5}' | sort -n | tail -n 1 | cut -d "%" -f1)
+
+	if (( $percentDisk > $DISK_CRITICAL ))
         then
-                send_error "CRITICAL" "HD is at $percentDisk %."
-        elif [ "$percentDisk" > $DISK_WARNING  ]
+                send_error " 🔴 CRITICAL 🔴 " "HD is at $percentDisk %."
+        elif (( $percentDisk > $DISK_WARNING  ))
 	then
-                send_error "WARNING" "HD is at $percentDisk %."
+                send_error " ❕ WARNING ❕ " "HD is at $percentDisk %."
 	fi
 
 
 	# Debug mode
-	if [ $DEBUG_MODE == 1 ]
+	if (( $DEBUG_MODE == 1 ))
         then
-                send_error "DEBUG" "The disk has been checked: $percentDisk %."
+                send_error " 📢 DEBUG 📢 " "The disk has been checked: $percentDisk %."
                 echo "DEBUG - The disk has been checked: $percentDisk %."
         fi
 
 }
+
 
 check_cpu(){
 
@@ -115,52 +115,46 @@ check_cpu(){
            sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
            awk '{print 100 - $1}')
 
-	freeCPU=$(top -bn1 | grep "Cpu(s)" | \
-           sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | \
-           awk '{print $1}')
-
-    #convert to int
-    usedCPUint=${usedCPU%.*}
-
-    if (( $usedCPUint > $CPU_CRITICAL ))
+    if (( ${usedCPU%.*} > $CPU_CRITICAL ))
         then
         	send_error "🔴 CRITICAL 🔴" "CPU is at $usedCPU %."
-        elif (( $usedCPUint > $CPU_WARNING ))
+        elif (( ${usedCPU%.*} > $CPU_WARNING ))
 		then
-        	send_error "WARNING" "CPU is at $usedCPU %."
+        	send_error " ❕ WARNING ❕ " "CPU is at $usedCPU %."
 	fi
 
+
 	# Debug mode
-        if [ $DEBUG_MODE == 1 ]
-        then
-                send_error "DEBUG" "The CPU has been checked: $usedCPU %."
-                echo "DEBUG - The CPU has been checked: $usedCPU %."
-        fi
+    if (( $DEBUG_MODE == 1 ))
+    then
+            send_error " 📢 DEBUG 📢 " "The CPU has been checked: $usedCPU %."
+            echo "DEBUG - The CPU has been checked: $usedCPU %."
+    fi
 }
+
 
 check_ping(){
 
 	hosts=$(echo $PING_HOSTS | tr ";" "\n")
 
-        for host in $hosts
-        do
+    for host in $hosts
+    do
 
-		ping=$(ping -i 0.2 -c $PING_NUMBER $host > /dev/null)
-
-		if [ "$ping" != 0 ]
+		if ! ping -c $PING_NUMBER $host > /dev/null 
 		then
-			send_error "CRITICAL" "Ping to $host has failed." 
+			send_error " 🔴 CRITICAL 🔴 " "Ping to $host has failed." 
 		fi
 
 
 		# Debug mode
-		if [ $DEBUG_MODE == 1 ]
-        	then
-                	send_error "DEBUG" "The ping to $host has been checked."
-                	echo "DEBUG - The ping to $host has been checked."
-        	fi
+		if (( $DEBUG_MODE == 1 ))
+    	then
+            	send_error " 📢 DEBUG 📢 " "The ping to $host has been checked."
+            	echo "DEBUG - The ping to $host has been checked."
+    	fi
 	done
 }
+
 
 check_processes(){
 
@@ -170,34 +164,42 @@ check_processes(){
 	do
 
 		running=$(ps ax | grep -v grep | grep $proc | wc -l)
-		if [ $running -le 0 ]
+
+		if (( $running <= 0 ))
 		then
-			send_error "CRITICAL" "The $proc process is not running."
+			send_error " 🔴 CRITICAL 🔴 " "The $proc process is not running."
 		fi
 
-		if [ $DEBUG_MODE == 1 ]
+
+		# Debug mode
+		if (( $DEBUG_MODE == 1 ))
 		then
-			send_error "DEBUG" "The $proc process has been checked."
+			send_error " 📢 DEBUG 📢 " "The $proc process has been checked."
 			echo "DEBUG - The $proc process has been checked."
 		fi
 	done
 }
 
+
 check_iptables(){
 
 	iptablesUP=$(sudo iptables -n -L -v --line-numbers | egrep "^[0-9]" | wc -l)
-	if [ $iptablesUP -le 0 ]
+
+	if (( $iptablesUP > 0 ))
         then
-        	send_error "CRITICAL" "Iptables rules are disabled."
+        	send_error " 🔴 CRITICAL 🔴 " "Iptables rules are disabled."
         fi
 
-        if [ $DEBUG_MODE == 1 ]
+
+        # Debug mode
+        if (( $DEBUG_MODE == 1 ))
         then
-        	send_error "DEBUG" "It has been checked if the iptables are disabled."
-                echo "DEBUG - It has been checked if the iptables are disabled."
+        	send_error " 📢 DEBUG 📢 " "It has been checked if the iptables are disabled."
+            echo "DEBUG - It has been checked if the iptables are disabled."
         fi
 
 }
+
 
 check_ports(){
 
@@ -205,41 +207,38 @@ check_ports(){
 
 	for port in $ports
 	do
-
-		hostP=$(echo $port | tr ":" " ")
-
-		portTest=$(nc -zv $hostP 2>&1)
-
-		if [[ $portTest != *"succeeded"* ]]; then
-  			send_error "CRITICAL" "The port [$port] is not open."
+		if ! netstat -l | grep $port | grep LISTEN > /dev/null
+		then
+  			send_error " 🔴 CRITICAL 🔴 " "The port [$port] is not open."
 		fi
 
-		if [ $DEBUG_MODE == 1 ]
+
+		# Debug mode
+		if (( $DEBUG_MODE == 1 ))
 		then
-			send_error "DEBUG" "The port [$port] has been checked."
+			send_error " 📢 DEBUG 📢 " "The port [$port] has been checked."
 			echo "DEBUG - The port [$port] has been checked."
 		fi
 	done
 }
 
-send_error(){
 
+send_error(){
 	message="$1 - $2"
 	$(curl -s -X POST $TELEGRAM_ENDPOINT -F text="$message" > /dev/null)
-
 }
 
 check() {
 
 	load_config # works
-	check_ram #works
-	check_swap #testing
-	check_disk #testing
-	check_cpu #works
-	check_ping #testing
-	check_processes #testing
-	check_iptables #testing
-	check_ports #testing
+	check_ram # works
+	check_swap # works
+	check_disk # works
+	check_cpu # works
+	check_ping # works
+	check_processes # works
+	check_iptables # works
+	check_ports # works
 
 }
 
